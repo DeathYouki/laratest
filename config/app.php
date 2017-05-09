94f5ff1ee38a12c69e089164f405927295837c79
Youki Sat <deathyouki@gmail.com>
2017-05-09 11:30:51 +0100

    New project, created it beceause image upload wasn't working on previous project

--- config/app.php
+++ config/app.php
@@ -176,8 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        //Added by Me
-        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -227,9 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        //Added by Me
-        'Form'  => Collective\Html\FormFacade::class,
-        'Html'  => Collective\Html\HtmlFacade::class,
 
     ],
 
